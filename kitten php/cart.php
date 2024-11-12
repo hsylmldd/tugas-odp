935e3614a73e122ebbd9981b507a753736469f94
hsylmldd <hasyamaulidaa16@gmail.com>
2024-11-12 12:33:12 +0700

    Delete kitten php

--- kitten php/cart.php
+++
@@ -1,7 +0,0 @@
-<!-- <section id="cart" class="cart-section py-5">
-    <div class="container">
-        <h2 class="text-center">Your Cart</h2> -->
-        <!-- Tambahkan konten keranjang di sini -->
-        <!-- <p>Your cart is currently empty.</p>
-    </div>
-</section> -->
