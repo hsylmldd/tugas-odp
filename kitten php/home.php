357f1b3b97daed5fd099e9c518110322fda4bbec
hsylmldd <hasyamaulidaa16@gmail.com>
2024-11-12 12:14:12 +0700

    upload tugas

---
+++ kitten php/home.php
@@ -0,0 +1,12 @@
+<section id="home" class="hero-section text-white">
+        <div class="container d-flex align-items-center">
+            <div class="hero-content w-50">
+                <h1>HIGH QUALITY <br>PET SHOP</h1>
+                <p class="lead">Sale up to 40% off today</p>
+                <a href="#shop" class="btn btn-dark btn-lg">Shop Now</a>
+            </div>
+            <!-- <div class="hero-image w-50">
+                <img src="assets/cat.png" alt="Dog Image" class="img-fluid">
+            </div> -->
+        </div>
+    </section>
